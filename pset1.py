a0d1499b191216f25757d0866d6132e7a796d308
Nuttt13 <manturyan@gmail.com>
2017-04-14 21:41:55 +0300

    Part A resolved

--- pset1.py
+++ pset1.py
@@ -5,26 +5,21 @@ Created on Wed Apr 12 21:12:31 2017
 
 @author: Enund
 """
-#num_month = int (input('Number of month: '))
-#down_payment = int ()
-
-current_savings = 0
-r = 0.04
-count = 0
-#invest = current_savings*r/12
-#down_payment = int(cost_home/4)
-#monthly_salary = annual_salary/12
-
 
 annual_salary = int(input('Enter your annual salary: '))
 save_per = float(input('Enter the percent of your salary to save, as a decimal: '))
 cost_home = int (input('Enter the cost of your dream home: '))
 
-invest = current_savings*r/12
 down_payment = cost_home/4
+current_savings = 0
+r = 0.04
+month = 0
+
 monthly_salary = annual_salary/12
 monthly_saved = monthly_salary*save_per
+while current_savings <= down_payment:
+      current_savings += monthly_salary*save_per + current_savings*r/12
+      month += 1
 
-total_saved = (monthly_saved + invest)
-num_month = int(total_saved/down_payment)
-print('Number of month: ', num_month)
+print('Number of month: ', month)
+
